50f971742fc3185affacb4c296fddaad3afbefbf
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-05-25 12:07:13 +0200

    MavenTp00

---
+++ src/main/java/org/fr/diginamic/App.java
@@ -0,0 +1,13 @@
+package org.fr.diginamic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
